691a8fe3e0cf50d6442d2a7469c9b9f0e9df39ee
David Torralbo <david@dtp.cat>
2019-10-06 21:22:50 +0200

    Update config file

--- config/config.php
+++ config/config.php
@@ -1,8 +1,20 @@
 <?php
 
 /*
- * You can place your custom package configuration in here.
+ * Feel free to modify Duck Funk CMS parameters here.
  */
 return [
 
+    /*
+     * The route the CMS core will get to build the website.
+     */
+    'route' => "/",
+
+    /*
+     * The view template the CMS core will get to build the website.
+     *
+     * As default, Duck Funk Core will get fuse template.
+     */
+
+    'template' => "fuse"
 ];
