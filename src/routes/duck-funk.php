17bc67071c7c717103b52874540d9b0bb6ab8560
David Torralbo <david@dtp.cat>
2019-10-13 21:48:14 +0200

    Modify route

--- src/routes/duck-funk.php
+++ src/routes/duck-funk.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['web', 'auth'], 'namespace' => 'Torralbodavid\DuckFunkCore\Http\Controllers'], function () {
     Route::prefix(config('duck-funk.route'))->group(function () {
-        Route::get('duck', 'DuckController')->name('duck');
+        Route::get('home', 'DuckController')->name('duck');
         Route::get('hello', 'TestController@hello')->name('hello');
     });
 });
