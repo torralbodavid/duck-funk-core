6e259802555996a606325c9a407631e0bb53edda
David Torralbo <david@dtp.cat>
2019-10-28 22:00:21 +0100

    Add RCONSocket class and RCONConnection trait

---
+++ src/Http/Traits/RCONConnection.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Torralbodavid\DuckFunkCore\Http\Traits;
+
+
+use Torralbodavid\DuckFunkCore\Http\RCON\RCONSocket;
+
+trait RCONConnection
+{
+    /**
+     * Send a alert to a online user
+     * @param string $message
+     * @param int $user_id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public static function alertUser(string $message = "Please, configure a message first", int $user_id = 2)
+    {
+        return (new RCONSocket(array(
+            'key'   => "AlertUser",
+            'data'  => array(
+                'message'   => $message,
+                'user_id'   => $user_id
+            )
+        )))->run();
+    }
+}
