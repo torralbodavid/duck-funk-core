13643f67690ee703eca30bb888bb8417a6b947dc
David Torralbo Pérez <davidtorralboperez@gmail.com>
2019-10-28 22:00:37 +0100

    Apply fixes from StyleCI (#8)

--- src/Http/Traits/RCONConnection.php
+++ src/Http/Traits/RCONConnection.php
@@ -1,27 +1,25 @@
 <?php
 
-
 namespace Torralbodavid\DuckFunkCore\Http\Traits;
-
 
 use Torralbodavid\DuckFunkCore\Http\RCON\RCONSocket;
 
 trait RCONConnection
 {
     /**
-     * Send a alert to a online user
+     * Send a alert to a online user.
      * @param string $message
      * @param int $user_id
      * @return \Illuminate\Http\JsonResponse
      */
-    public static function alertUser(string $message = "Please, configure a message first", int $user_id = 2)
+    public static function alertUser(string $message = 'Please, configure a message first', int $user_id = 2)
     {
-        return (new RCONSocket(array(
-            'key'   => "AlertUser",
-            'data'  => array(
+        return (new RCONSocket([
+            'key'   => 'AlertUser',
+            'data'  => [
                 'message'   => $message,
-                'user_id'   => $user_id
-            )
-        )))->run();
+                'user_id'   => $user_id,
+            ],
+        ]))->run();
     }
 }
