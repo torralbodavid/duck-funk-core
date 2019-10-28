cce4138050fa2a89c7fc879d23493c7757f3d2f6
David Torralbo <david@dtp.cat>
2019-10-28 21:57:48 +0100

    Add emulator and RCON connection params

--- config/config.php
+++ config/config.php
@@ -20,6 +20,20 @@ return [
      *
      * As default, Duck Funk Core will get fuse template.
      */
-
     'template' => 'fuse',
+
+    'hotel' => 'https://www.habbo.com/',
+
+    /*
+     * Emulator connection params
+     */
+    'host_server' => '127.0.0.1',
+    'port_server' => '3000',
+
+    /*
+     * RCON connection params
+     */
+    'host_rcon' => '127.0.0.1',
+    'port_rcon' => '3001',
+    'auth_key_rcon' => env('RCON_AUTH_KEY', 'IA1LLX4QZI7U63AKQEULQ3D84SYAKTO7')
 ];
