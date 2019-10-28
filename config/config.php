23619748ac72f4e38216b4acb61b646263a0ea65
David Torralbo Pérez <davidtorralboperez@gmail.com>
2019-10-28 21:58:07 +0100

    Apply fixes from StyleCI (#7)

--- config/config.php
+++ config/config.php
@@ -35,5 +35,5 @@ return [
      */
     'host_rcon' => '127.0.0.1',
     'port_rcon' => '3001',
-    'auth_key_rcon' => env('RCON_AUTH_KEY', 'IA1LLX4QZI7U63AKQEULQ3D84SYAKTO7')
+    'auth_key_rcon' => env('RCON_AUTH_KEY', 'IA1LLX4QZI7U63AKQEULQ3D84SYAKTO7'),
 ];
