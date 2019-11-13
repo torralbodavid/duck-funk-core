bed9c9b50f4fc9cf92b98bff6875bc4bb4d6a8ef
David Torralbo <david@dtp.cat>
2019-11-13 19:09:28 +0100

    Route controller refactor

--- src/routes/duck-funk.php
+++ src/routes/duck-funk.php
@@ -1,41 +1,33 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use \Torralbodavid\DuckFunkCore\Http\Middleware\BanMiddleware;
 
-/*
- *
- */
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
 
-/*
- * All routes inside here requires a login
- */
-Route::group(['middleware' => ['web', 'auth', \Torralbodavid\DuckFunkCore\Http\Middleware\BanMiddleware::class], 'namespace' => 'Torralbodavid\DuckFunkCore\Http\Controllers'], function () {
+Route::group(['middleware' => ['web'], 'namespace' => 'App\Http\Controllers'], function () {
+    Auth::routes();
+    Route::get('logout', 'Auth\LoginController@logout');
+});
+
+Route::group(['middleware' => ['web', 'auth', BanMiddleware::class], 'namespace' => 'Torralbodavid\DuckFunkCore\Http\Controllers'], function () {
     /*
      * Check where we start to construct the routes based in the config route param
      */
     Route::prefix(config('duck-funk.route'))->group(function () {
 
-        /*
-         * Those routes user will see if has logged in but has no avatar selected
-         */
         Route::get('home', 'DuckController')->name('home');
-        Route::get('hello', 'TestController@hello')->name('hello');
         Route::get('expulsion', function () {
             return view('duck-funk-core::ban');
         })->name('ban');
-
-        /*
-         * Avatar settings
-         */
-        Route::prefix('avatars')->group(function () {
-            Route::get('select', 'AvatarController@select')->name('avatarSelect');
-            Route::get('login', 'AvatarLoginController@login')->name('avatarLogin');
-        });
 
         Route::get('hotel', 'GameController@showHotel')->name('hotel');
     });
 });
 
-Route::fallback(function () {
-    abort(404, 'Page not found');
+Route::fallback(function (){
+    abort(404);
 });
