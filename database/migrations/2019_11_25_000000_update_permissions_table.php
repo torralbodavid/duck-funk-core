0f82732ad02d33b90edb0a64fd1516164e2745c8
David Torralbo <david@dtp.cat>
2019-11-25 19:12:21 +0100

    Add update permissions table migration

---
+++ database/migrations/2019_11_25_000000_update_permissions_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePermissionsTable extends \Illuminate\Database\Migrations\Migration
+{
+    public function up()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->boolean('duck_funk_housekeeping_read');
+            $table->boolean('duck_funk_housekeeping_write');
+            $table->boolean('duck_funk_housekeeping_approve');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->dropColumn('duck_funk_housekeeping_read');
+            $table->dropColumn('duck_funk_housekeeping_write');
+            $table->dropColumn('duck_funk_housekeeping_approve');
+        });
+    }
+}
