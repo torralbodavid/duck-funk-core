9a202f0d579f98faa58aa002a8732d6984c9928d
David Torralbo <david@dtp.cat>
2019-11-25 21:04:12 +0100

    Add housekeeping controller

---
+++ src/Http/Controllers/Housekeeping/HousekeepingController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Torralbodavid\DuckFunkCore\Http\Controllers\Housekeeping;
+
+
+class HousekeepingController
+{
+    public function __invoke()
+    {
+        return "housekeeping";
+    }
+}
