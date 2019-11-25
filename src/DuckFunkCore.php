5511cd1ab3f2eeec672ed0bcf2ee3b65ee779b2c
David Torralbo <david@dtp.cat>
2019-11-25 21:04:34 +0100

    Add getUser and canSeeHousekeeping static functions

--- src/DuckFunkCore.php
+++ src/DuckFunkCore.php
@@ -2,10 +2,23 @@
 
 namespace Torralbodavid\DuckFunkCore;
 
+use Illuminate\Support\Facades\Auth;
+use Torralbodavid\DuckFunkCore\Models\Arcturus\User;
+
 class DuckFunkCore
 {
     /*
      * Package version
      */
     const PACKAGE_VERSION = '0.0.2';
+
+    public static function getUser()
+    {
+        return User::find(Auth::id());
+    }
+
+    public static function canSeeHousekeeping()
+    {
+        return self::getUser()->permissions->canReadHousekeeping();
+    }
 }
