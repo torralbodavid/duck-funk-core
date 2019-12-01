f7a4394153204a200af1e0dcba7f36d6604c6ea9
torralbodavid <davidtorralboperez@gmail.com>
2019-12-01 22:08:47 +0000

    Apply fixes from StyleCI

--- src/Http/Middleware/HousekeepingMiddleware.php
+++ src/Http/Middleware/HousekeepingMiddleware.php
@@ -4,13 +4,13 @@ namespace Torralbodavid\DuckFunkCore\Http\Middleware;
 
 use Closure;
 use phpDocumentor\Reflection\Types\Collection;
-use Torralbodavid\DuckFunkCore\DuckFunkCore;
 
 class HousekeepingMiddleware
 {
     public function handle($request, Closure $next)
     {
         dd($this->constructMenu());
+
         return core()->canSeeHousekeeping() ? $next($request) : redirect('home')->with('status', 403);
     }
 
@@ -28,8 +28,8 @@ class HousekeepingMiddleware
                     'link' => route('housekeeping'),
                     'target' => '_self', // todo: make self default and avoid to write if is not it's _blank
                     'icon' => 'ion ion-md-speedometer',
-                    'submenu' => 'false' // todo: make false default and avoid to write if there is no submenu
-                ]
+                    'submenu' => 'false', // todo: make false default and avoid to write if there is no submenu
+                ],
             ],
             'moderation' => [
                 'item' => [
@@ -37,9 +37,9 @@ class HousekeepingMiddleware
                     'link' => route('housekeeping'),
                     'target' => '_self', // todo: make self default and avoid to write if is not it's _blank
                     'icon' => 'ion ion-md-speedometer',
-                    'submenu' => 'false' // todo: make false default and avoid to write if there is no submenu
-                ]
-            ]
+                    'submenu' => 'false', // todo: make false default and avoid to write if there is no submenu
+                ],
+            ],
         ]);
 
         return $menu->all();
