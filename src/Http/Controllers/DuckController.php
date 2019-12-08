bef00df78278fa808b5dd3f1f4693e5fc6d22c73
David Torralbo Pérez <davidtorralboperez@gmail.com>
2019-12-08 19:10:16 +0100

    Apply fixes from StyleCI (#22)

--- src/Http/Controllers/DuckController.php
+++ src/Http/Controllers/DuckController.php
@@ -3,13 +3,11 @@
 namespace Torralbodavid\DuckFunkCore\Http\Controllers;
 
 use Illuminate\Routing\Controller;
-use Torralbodavid\DuckFunkCore\Models\Arcturus\User;
 
 class DuckController extends Controller
 {
     public function __invoke()
     {
-
         return view('duck-funk-core::hello', ['hello' => core()->user()->permissions->name]);
     }
 }
