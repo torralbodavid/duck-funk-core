3755cea69e6bb7beda4d1f85863c42d8dc2980f4
David Torralbo <david@dtp.cat>
2019-12-12 18:58:24 +0100

    Add new factories

---
+++ database/factories/Arcturus/UserFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+use Torralbodavid\DuckFunkCore\Models\Arcturus\User;
+
+$factory->define(User::class, function (Faker $faker) {
+    return [
+        'username' => $faker->userName,
+        'mail' => $faker->email,
+        'password' => $faker->password(6, 64),
+        'account_created' => $faker->unixTime,
+        'ip_register' => $faker->ipv4,
+        'ip_current' => $faker->ipv4
+    ];
+});
