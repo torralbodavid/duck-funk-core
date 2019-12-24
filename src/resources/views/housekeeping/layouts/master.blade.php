124cb5e8455c760193537e6e9f7e8e14331f463e
David Torralbo <david@dtp.cat>
2019-12-24 17:48:35 +0100

    Refactor labs dashboard

---
+++ src/resources/views/housekeeping/layouts/master.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('housekeeping::partials.head')
+
+<!-- datepicker -->
+    <link href="{{ mix('css/bootstrap-datepicker.min.css', '/vendor/duck-funk-core') }}" rel="stylesheet" type="text/css" />
+
+    <!-- jvectormap -->
+    <link href="{{ mix('css/jquery-jvectormap-2.0.2.css', '/vendor/duck-funk-core') }}" rel="stylesheet" type="text/css" />
+
+    <link href="{{ mix('css/bootstrap.min.css', '/vendor/duck-funk-core') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ mix('css/metismenu.min.css', '/vendor/duck-funk-core') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ mix('css/icons.css', '/vendor/duck-funk-core') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ mix('css/housekeeping/style.css', '/vendor/duck-funk-core') }}" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+@yield('content')
+
+@yield('scripts')
+
+</body>
+
+</html>
