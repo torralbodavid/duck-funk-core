4650bdbd8d06a68fdfa53a6f2c87ca90a7b7a41e
torralbodavid <davidtorralboperez@gmail.com>
2020-01-10 14:51:09 +0000

    Apply fixes from StyleCI

--- src/Http/RCON/RCONSocket.php
+++ src/Http/RCON/RCONSocket.php
@@ -40,7 +40,7 @@ class RCONSocket
               [
                   'connection' => 'failed',
                   'error'   => $exception->getMessage(),
-                  ]
+              ]
             );
         }
 
