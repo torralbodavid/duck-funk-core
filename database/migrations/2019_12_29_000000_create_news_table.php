26bd9b4b0b71a31d97168c1729632b70fac4da31
David Torralbo <david@dtp.cat>
2020-01-11 20:20:46 +0100

    Add create news migration

---
+++ database/migrations/2019_12_29_000000_create_news_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('duck_funk_news', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 155);
+            $table->string('subtitle', 255);
+            $table->text('body');
+            $table->json('categories');
+            $table->string('image_link', 255)->nullable();
+            $table->integer('hotelview_news_id')->default(null)->nullable();
+            $table->boolean('draft')->default(false);
+            $table->unsignedBigInteger('author');
+            $table->timestamps();
+            $table->timestamp('published_at')->nullable();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('duck_funk_news');
+    }
+}
