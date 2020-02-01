059cc050600116393832a3b5c5bb588981364256
David Torralbo <david@dtp.cat>
2020-02-01 14:41:46 +0100

    Add api routes

---
+++ src/routes/duck-funk-api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Torralbodavid\DuckFunkCore\Http\Middleware\BanMiddleware;
+
+Route::prefix('api')->group(function () {
+
+    Route::prefix('newuser')->group(function () {
+        Route::group(['middleware' => ['web', 'auth', BanMiddleware::class]], function () {
+
+            Route::prefix('name')->group(function () {
+                Route::post('check', [\Torralbodavid\DuckFunkCore\Http\Controllers\API\Welcome\WelcomeController::class, 'check']);
+            });
+
+        });
+    });
+
+});
