590cd73b5b3e6148b515f39964c03661ce3f428d
David Torralbo <david@dtp.cat>
2020-02-02 11:58:29 +0100

    Add new routes at duck funk api route list

--- src/routes/duck-funk-api.php
+++ src/routes/duck-funk-api.php
@@ -4,11 +4,10 @@ use Illuminate\Support\Facades\Route;
 use Torralbodavid\DuckFunkCore\Http\Middleware\BanMiddleware;
 
 Route::prefix('api')->group(function () {
-    Route::prefix('newuser')->group(function () {
-        Route::group(['middleware' => ['web', 'auth', BanMiddleware::class]], function () {
-            Route::prefix('name')->group(function () {
-                Route::post('check', [\Torralbodavid\DuckFunkCore\Http\Controllers\API\Welcome\WelcomeController::class, 'check']);
-            });
-        });
+    Route::group(['middleware' => ['web', 'auth', BanMiddleware::class]], function () {
+        Route::post('newuser/name/check', [\Torralbodavid\DuckFunkCore\Http\Controllers\API\Welcome\WelcomeController::class, 'check']);
+        Route::post('newuser/name/select', [\Torralbodavid\DuckFunkCore\Http\Controllers\API\Welcome\WelcomeController::class, 'select']);
+        Route::post('newuser/room/select', [\Torralbodavid\DuckFunkCore\Http\Controllers\API\Welcome\WelcomeController::class, 'roomSelect']);
+        Route::post('user/look/save', [\Torralbodavid\DuckFunkCore\Http\Controllers\API\Welcome\WelcomeController::class, 'save']);
     });
 });
