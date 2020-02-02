e5a5cf6752ae6b6f10fae9120bcae57f9c5f204e
David Torralbo <david@dtp.cat>
2020-02-02 11:58:19 +0100

    Add room and item models

---
+++ src/Models/Arcturus/Item.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Torralbodavid\DuckFunkCore\Models\Arcturus;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    protected $table = 'items';
+    protected $guarded = [];
+    public $timestamps = false;
+}
---
+++ src/Models/Arcturus/Room.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Torralbodavid\DuckFunkCore\Models\Arcturus;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+    protected $table = 'rooms';
+    protected $guarded = [];
+    public $timestamps = false;
+}
