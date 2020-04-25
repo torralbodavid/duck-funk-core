f7b86e2a90f646a46460165babebc42d94de1c6a
David Torralbo <david@dtp.cat>
2020-04-25 18:48:25 +0200

    Delete filepond scripts

--- webpack.mix.js
+++ webpack.mix.js
@@ -64,12 +64,6 @@ mix.options({
     .sass('src/resources/assets/scss/style.scss', 'public/css')
     .sass('src/resources/assets/housekeeping-scss/style.scss', 'public/css/housekeeping')
     .js('src/resources/assets/js/duck_funk/labs/*', 'public/js/housekeeping/legacy/labs.js')
-    .js('src/resources/assets/js/filepond.js', 'public/js/legacy')
-    .styles([
-        'src/resources/assets/filepond/filepond.css',
-        'src/resources/assets/filepond/filepond-plugin-image-edit.css',
-        'src/resources/assets/filepond/filepond-plugin-image-preview.css'
-    ], 'public/css/filepond.css')
     .purgeCss()
     .version()
     .webpackConfig({
