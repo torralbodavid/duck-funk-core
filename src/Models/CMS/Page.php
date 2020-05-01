d1e7de669004f6994d2e4a3bf07bbba826cf2bc2
David Torralbo <david@dtp.cat>
2020-05-01 16:40:36 +0200

    Add soft delete at page model

--- src/Models/CMS/Page.php
+++ src/Models/CMS/Page.php
@@ -3,11 +3,14 @@
 namespace Torralbodavid\DuckFunkCore\Models\CMS;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Torralbodavid\DuckFunkCore\Scopes\ActiveScope;
 use Torralbodavid\DuckFunkCore\Scopes\PublishedScope;
 
 class Page extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'duck_funk_pages';
     protected $guarded = [];
 
