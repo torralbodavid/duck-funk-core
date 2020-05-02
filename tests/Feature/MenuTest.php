2ef86f56a919f5b632865f780d86c7a838207840
David Torralbo <david@dtp.cat>
2020-05-02 23:12:41 +0200

    Menu acts as singleton

---
+++ tests/Feature/MenuTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Torralbodavid\DuckFunkCore\Tests\Feature;
+
+use Torralbodavid\DuckFunkCore\Models\Arcturus\User;
+use Torralbodavid\DuckFunkCore\Tests\TestCase;
+
+class MenuTest extends TestCase
+{
+    /** @test */
+    public function menu_acts_as_singleton()
+    {
+        $this->assertSame(menu(), menu());
+        $this->assertSame(spl_object_id(menu()), spl_object_id(menu()));
+    }
+}
