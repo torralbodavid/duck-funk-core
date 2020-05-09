b3a798f8a79806887a49bde7430edc1dd9bbd46b
torralbodavid <davidtorralboperez@gmail.com>
2020-05-09 22:57:53 +0000

    Apply fixes from StyleCI

--- src/Services/Captcha.php
+++ src/Services/Captcha.php
@@ -4,9 +4,9 @@ namespace Torralbodavid\DuckFunkCore\Services;
 
 class Captcha
 {
-    protected const SERVICE_URL = "https://www.google.com/recaptcha/api/siteverify";
-    protected string $secret = "";
-    protected $response = "";
+    protected const SERVICE_URL = 'https://www.google.com/recaptcha/api/siteverify';
+    protected string $secret = '';
+    protected $response = '';
 
     public function __construct($recaptcha_response)
     {
@@ -16,7 +16,8 @@ class Captcha
 
     public function getResponse()
     {
-        $recaptcha = file_get_contents(self::SERVICE_URL . '?secret=' . $this->secret . '&response=' . $this->response);
+        $recaptcha = file_get_contents(self::SERVICE_URL.'?secret='.$this->secret.'&response='.$this->response);
+
         return json_decode($recaptcha);
     }
 }
