efc83fbafa6eea532a91d583fc5efb19f0deeb25
David Torralbo <davidtorralboperez@gmail.com>
2020-07-19 23:26:03 +0200

    Update js

--- src/resources/assets/js/app.js
+++ src/resources/assets/js/app.js
@@ -1,104 +1,20 @@
-/*
- Template Name: Veltrix - Responsive Bootstrap 4 Admin Dashboard
- Author: Themesbrand
- Website: www.themesbrand.com
- File: Main js
- */
+require('./bootstrap');
 
+document.addEventListener('DOMContentLoaded', function() {
 
-!function($) {
-    "use strict";
+    var Turbolinks = require("turbolinks")
+    Turbolinks.start()
 
-    var MainApp = function() {
-        this.$btnFullScreen = $("#btn-fullscreen")
-    };
+    const menus = document.querySelectorAll('.navbar-burger');
+    const dropdowns = document.querySelectorAll('.navbar-menu');
 
-    MainApp.prototype.initNavbar = function () {
-
-        $('.navbar-toggle').on('click', function (event) {
-            $(this).toggleClass('open');
-            $('#navigation').slideToggle(400);
-        });
-
-        $('.navigation-menu>li').slice(-2).addClass('last-elements');
-
-        $('.navigation-menu li.has-submenu a[href="#"]').on('click', function (e) {
-            if ($(window).width() < 992) {
-                e.preventDefault();
-                $(this).parent('li').toggleClass('open').find('.submenu:first').toggleClass('open');
-            }
-        });
-    },
-
-        MainApp.prototype.initSlimscroll = function () {
-            $('.slimscroll').slimscroll({
-                height: 'auto',
-                position: 'right',
-                size: "7px",
-                color: '#9ea5ab',
-                touchScrollStep: 50
-            });
-        },
-
-        MainApp.prototype.initMenuItem = function () {
-            $(".navigation-menu a").each(function () {
-                var pageUrl = window.location.href.split(/[?#]/)[0];
-                if (this.href == pageUrl) {
-                    $(this).parent().addClass("active"); // add active to li of the current link
-                    $(this).parent().parent().parent().addClass("active"); // add active class to an anchor
-                    $(this).parent().parent().parent().parent().parent().addClass("active"); // add active class to an anchor
+    if (menus.length && dropdowns.length) {
+        for (var i = 0; i < menus.length; i++) {
+            menus[i].addEventListener('click', function() {
+                for (var j = 0; j < dropdowns.length; j++) {
+                    dropdowns[j].classList.toggle('hidden');
                 }
             });
-        },
-
-        MainApp.prototype.initComponents = function () {
-            $('[data-toggle="tooltip"]').tooltip();
-            $('[data-toggle="popover"]').popover();
-        },
-
-        //full screen
-        MainApp.prototype.initFullScreen = function () {
-            var $this = this;
-            $this.$btnFullScreen.on("click", function (e) {
-                e.preventDefault();
-
-                if (!document.fullscreenElement && /* alternative standard method */ !document.mozFullScreenElement && !document.webkitFullscreenElement) {  // current working methods
-                    if (document.documentElement.requestFullscreen) {
-                        document.documentElement.requestFullscreen();
-                    } else if (document.documentElement.mozRequestFullScreen) {
-                        document.documentElement.mozRequestFullScreen();
-                    } else if (document.documentElement.webkitRequestFullscreen) {
-                        document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-                    }
-                } else {
-                    if (document.cancelFullScreen) {
-                        document.cancelFullScreen();
-                    } else if (document.mozCancelFullScreen) {
-                        document.mozCancelFullScreen();
-                    } else if (document.webkitCancelFullScreen) {
-                        document.webkitCancelFullScreen();
-                    }
-                }
-            });
-        },
-
-
-
-        MainApp.prototype.init = function () {
-            this.initNavbar();
-            this.initSlimscroll();
-            this.initMenuItem();
-            this.initComponents();
-            this.initFullScreen();
-            Waves.init();
-        },
-
-        //init
-        $.MainApp = new MainApp, $.MainApp.Constructor = MainApp
-}(window.jQuery),
-
-//initializing
-    function ($) {
-        "use strict";
-        $.MainApp.init();
-    }(window.jQuery);
+        }
+    }
+});
